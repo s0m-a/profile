34dff6c9908ab296ad0c1c6301e8a0cfd86406dc
s0m-a <ogbennaanita@gmail.com>
2025-02-21 16:45:33 +0100

    fixing acesstoken bug

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const allowedOrigins = [
     "http://localhost:3000",  // Allow localhost
     "http://172.20.10.13:3000", // Allow local network IP
-    "https://profile-84haycgai-somas-projects-250320be.vercel.app"
+    "https://profile-eight-brown.vercel.app"
     
   ];
   
