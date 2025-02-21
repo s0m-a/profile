0c4f1954dc0c580219191f6a59afe975b1766cab
s0m-a <ogbennaanita@gmail.com>
2025-02-21 06:00:28 +0100

    fixinf deployment bug

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,  // Keep React in strict mode for best practices
-    experimental: {
-      optimizeFonts: false, // Prevents Next.js from optimizing Google Fonts
-    },
   };
   
   export default nextConfig;
