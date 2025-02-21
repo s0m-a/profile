f0edfb29269f65263fb3c3e79784453a7947fd52
s0m-a <ogbennaanita@gmail.com>
2025-02-21 05:50:39 +0100

    fixing font

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //  reactStrictMode: false,
-};
-
-export default nextConfig;
+    reactStrictMode: true,  // Keep React in strict mode for best practices
+    experimental: {
+      optimizeFonts: false, // Prevents Next.js from optimizing Google Fonts
+    },
+  };
+  
+  export default nextConfig;
+  
